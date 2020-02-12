67b05e37a749bd95de69e2e5571cf6073d8d1875
Artur <artursuslov@gmail.com>
2020-02-12 17:30:07 +0200

    added checkbox item and styles

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,36 @@
 import React, { Component } from 'react';
+import Checkbox from './Checkbox';
 
-class Filer extends Component {
-    
+class Filter extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { 
+            label: {isChecked: false},
+            label2: {isChecked: true} 
+        };
+    }
+
+    handleCheckboxChange = event => {
+        console.log(event.target);
+    }
+  
+    render() {
+      return (
+        <div style={{ fontFamily: 'system-ui', 'width': '25%' }}>
+            <Checkbox
+                isChecked={this.state.label.isChecked}
+                name='Label'
+                onClick={this.handleCheckboxChange}
+            />
+            <Checkbox
+                isChecked={this.state.label2.isChecked}
+                name='Label2'
+                onClick={this.handleCheckboxChange}
+            />           
+        </div>
+      )
+    }
 }
+
+export default Filter;
