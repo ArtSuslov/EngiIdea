b7b81b10b6caecc152eb90ad15287445c895c418
Artur <artursuslov@gmail.com>
2020-02-17 05:15:30 +0200

    minor improvements

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,20 +4,21 @@ import Category from './Category';
 import styled from 'styled-components';
 
 const Container = styled.div`
-    overflow-y: scroll;
+    overflow-y: auto;
     height: 100vh;
     font-family: Roboto;
-    flex-basis: 82%;
+    flex-basis: calc(96% - 201px);
     padding-left: 2%;
     padding-right: 2%;
-    @media (max-width: 1024px) {
-        flex-basis: 76%;
-    };
-    @media (max-width: 768px) {
-        flex-basis: 69%;
-    };
-`
 
+`
+/*
+@media (max-width: 1024px) {
+    flex-basis: 76%;
+};
+@media (max-width: 768px) {
+    flex-basis: 69%;
+};*/
 class Content extends Component {
     renderContent () {
         const { categoriesList, isFetching } = this.props.content;
